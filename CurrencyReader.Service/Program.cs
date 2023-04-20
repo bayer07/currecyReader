2a61875eb1acd8c7ab76c42eb894f3ecf6b9478f
Bair Dongak <bayer07b@gmail.com>
2023-04-21 00:15:24 +0300

    Add CurrencyReader.Service project

---
+++ CurrencyReader.Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CurrencyReader.Service
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+
+            })
+            .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
+
